4663b4350b6c7bf11893398353d5c68a602e1dd9
PaddyAdallah <adalapaddy9@gmail.com>
2022-06-14 18:00:08 +0300

    Udacity Udagram app

--- src/server.ts
+++ src/server.ts
@@ -15,6 +15,30 @@ import {filterImageFromURL, deleteLocalFiles} from './util/util';
 
   // @TODO1 IMPLEMENT A RESTFUL ENDPOINT
   // GET /filteredimage?image_url={{URL}}
+ // FilterImage endpoint
+ app.get('/filteredimage', async (req: Request, res: Response) => {
+  // get the image_url parameter from the query
+  let { image_url } = req.query;
+  // parameter validation
+  if(!image_url){
+    res.status(400).send("image_url parameter is required");
+  }
+
+  try{
+    // image filtering processing
+    const filteredImage = await filterImageFromURL(image_url);
+    // sending the resulting file in the response
+    res.status(200).sendFile(filteredImage, () => {
+      // deleting any files on the server
+      deleteLocalFiles([filteredImage]);
+    });
+  }
+  catch(error){
+    // sending error to client in case image filtering processing failed
+    res.status(422).send("Error processing the media url");
+  }
+});
+  
   // endpoint to filter an image from a public url.
   // IT SHOULD
   //    1
